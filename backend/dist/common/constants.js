07b4c04005bdc05fa6dc415b608671a64b680af0
Keshav Raj <www.keshav.mars@gmail.com>
2024-08-10 22:30:06 +0530

    fixed untracked files

--- backend/dist/common/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMPTY_STRING = exports.RegexPatterns = void 0;
-exports.RegexPatterns = {
-    GSTIN: /\d{2}[A-Z]{5}\d{4}[A-Z]{1}[A-Z\d]{1}Z[A-Z\d]{1}/,
-    PASSWORD: /^(?=.*[A-Z])(?=.*[!@#$%^&*()-_=+\\|[\]{};:'",.<>/?])(?=.*[0-9]).{6,}$/,
-};
-exports.EMPTY_STRING = "";
-//# sourceMappingURL=constants.js.map
